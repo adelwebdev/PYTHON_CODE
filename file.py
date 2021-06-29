e4cae58c44e4e17c8a679b20600f6a125aea1289
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-29 17:01:25 +0300

    Add files via upload

---
+++ file.py
@@ -0,0 +1,18 @@
+print("word")
+num1 = input("enter the first number:  ")
+num2 = input("enter the second  number:  ")
+
+result = float(num1) + float(num2)
+print(result)
+
+color = input("enter a color: ")
+pluralNoun = input("enter a plural noun: " )
+celebrety = input("enter a celebrety: ")
+
+print ("roses are " + color)
+print (pluralNoun + "  are blue")
+print ("i love " + celebrety)
+
+# list 
+
+
