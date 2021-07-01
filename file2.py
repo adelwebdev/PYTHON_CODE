4c5682ccebce577d83bf3a0dbb3c71340dc16a23
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-01 22:51:44 +0300

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,8 @@
+# -----------------------------------FILES on PYTHON ----------------------------------------
+file_python = open("file.txt", "r")
+print(file_python.read()) 
+file_python.close() 
+
+file_python = open("file.txt", "a")
+file_python.write("tobby - human resources")
+file_python.close() 
