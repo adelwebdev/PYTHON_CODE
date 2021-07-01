fc4e8a369a5130d160d858c3e0c33cada394708a
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-01 23:12:21 +0300

    Add files via upload

--- file2.py
+++ file2.py
@@ -3,6 +3,10 @@ file_python = open("file.txt", "r")
 print(file_python.read()) 
 file_python.close() 
 
-file_python = open("file.txt", "a")
+file_python = open("file.txt", "w")
 file_python.write("tobby - human resources")
+file_python.close() 
+
+file_python = open("index.html", "w")
+file_python.write("<p>some paragraphe in html</p>")
 file_python.close() 
