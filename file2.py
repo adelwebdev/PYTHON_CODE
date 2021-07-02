97e358983b723ecaf2919330565ba3b07417a8d4
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-02 18:26:48 +0300

    Add files via upload

--- file2.py
+++ file2.py
@@ -1,3 +1,5 @@
+from Question import Question
+
 # -----------------------------------FILES on PYTHON ----------------------------------------
 file_python = open("file.txt", "r")
 print(file_python.read()) 
@@ -10,3 +12,25 @@ file_python.close()
 file_python = open("index.html", "w")
 file_python.write("<p>some paragraphe in html</p>")
 file_python.close() 
+
+question_prompts = [
+    "what color are apples? \n(a) Teal\n(b) Magenta "
+    "what color are bananas?\n(a) Teal\n(b) Magenta "
+    "what color are strawberries?\n(a) Teal\n(b) Magenta "
+]
+
+questions = [
+    Question(question_prompts[0],"a"),
+    Question(question_prompts[1],"a"),
+    Question(question_prompts[2],"b"),
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("you got " + str(score) + "/" + str(len(question)) + "correct")
+
+run_test(questions)   
