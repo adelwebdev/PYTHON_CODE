668264682c2aa3a2fbc933e025aba4940b0dae0c
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-02 18:25:22 +0300

    Add files via upload

---
+++ chef.py
@@ -0,0 +1,17 @@
+from useful import Chef
+# class ChineseChef:
+#     def make_chicken(self):
+#         print("The chef makes a chicken")
+
+#     def make_salad(self):
+#         print("The chef makes salad")     
+        
+#     def make_dish(self):
+#         print("The chef makes a bbq")  
+
+#     def make_rice(self):
+#         print("the chef makes rice")
+
+class ChineseChef(Chef):
+    def make_rice(self):
+        print("the chef makes rice")
